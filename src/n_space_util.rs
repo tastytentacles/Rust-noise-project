eec3f1a9b2a16a8d0a04c7930fc42433b86bd533
Tasty Tentacles <marq46.steam@googlemail.com>
2023-12-02 20:03:27 +0000

    doc

--- src/n_space_util.rs
+++ src/n_space_util.rs
@@ -4,6 +4,7 @@ use vecmath::Vector2;
 pub fn n_space_unwrap(n: usize, width: usize) -> Vector2<u32>
     { return [n as u32 % width as u32, f32::floor(n as f32 / width as f32) as u32] }
 
+// returns a vector2 uv position from a 1d index.
 // pub fn n_space_uv(n: usize, width: usize) -> Vector2<f32> {
 //     let n_space = n_space_unwrap(n, width);
     
