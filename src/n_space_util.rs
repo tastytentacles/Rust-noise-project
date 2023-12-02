2f7829243fa273d90149267b6db9be6201740fce
Tasty Tentacles <marq46.steam@googlemail.com>
2023-12-02 19:52:48 +0000

    simple doc

--- src/n_space_util.rs
+++ src/n_space_util.rs
@@ -1,13 +1,13 @@
 use vecmath::Vector2;
 
-
+// returns a vector2 grid position from a 1d index.
 pub fn n_space_unwrap(n: usize, width: usize) -> Vector2<u32>
     { return [n as u32 % width as u32, f32::floor(n as f32 / width as f32) as u32] }
 
-pub fn n_space_uv(n: usize, width: usize) -> Vector2<f32> {
-    let n_space = n_space_unwrap(n, width);
+// pub fn n_space_uv(n: usize, width: usize) -> Vector2<f32> {
+//     let n_space = n_space_unwrap(n, width);
     
-    return [
-        n_space[0] as f32 / width as f32,
-        n_space[0] as f32 / width as f32 ];
-}
+//     return [
+//         n_space[0] as f32 / width as f32,
+//         n_space[0] as f32 / width as f32 ];
+// }
